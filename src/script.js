adc7174884adcaa35abd82164597edf9ebeac758
Jo-Sa80 <j.sandvoss80@gmail.com>
2024-12-12 19:02:46 +0100

    set interval

--- src/script.js
+++ src/script.js
@@ -1,23 +1,30 @@
-let losAngelesElement = document.querySelector("#losAngeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesAmPmElement = losAngelesElement.querySelector("small");
-let losAngelesDate = moment().tz("America/Los_Angeles").format(" MMMM d YYYY");
-let losAngelesTime = moment().tz("America/Los_Angeles").format(" h:mm:ss");
-let losAngelesAmPm = moment().tz("America/Los_Angeles").format(" A");
+function updateTime() {
+  let losAngelesElement = document.querySelector("#losAngeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesAmPmElement = losAngelesElement.querySelector("small");
+  let losAngelesDate = moment()
+    .tz("America/Los_Angeles")
+    .format(" MMMM d YYYY");
+  let losAngelesTime = moment().tz("America/Los_Angeles").format(" h:mm:ss");
+  let losAngelesAmPm = moment().tz("America/Los_Angeles").format(" A");
 
-losAngelesDateElement.innerHTML = losAngelesDate;
+  losAngelesDateElement.innerHTML = losAngelesDate;
 
-losAngelesTimeElement.innerHTML = `${losAngelesTime} <small>${losAngelesAmPm}</small>`;
+  losAngelesTimeElement.innerHTML = `${losAngelesTime} <small>${losAngelesAmPm}</small>`;
 
-let parisElement = document.querySelector("#paris");
-let parisDateElement = parisElement.querySelector(".date");
-let parisTimeElement = parisElement.querySelector(".time");
-let parisAmPmElement = parisElement.querySelector("small");
-let parisDate = moment().tz("Europe/Paris").format(" MMMM d YYYY");
-let parisTime = moment().tz("Europe/Paris").format(" h:mm:ss");
-let parisAmPm = moment().tz("Europe/Paris").format(" A");
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisAmPmElement = parisElement.querySelector("small");
+  let parisDate = moment().tz("Europe/Paris").format(" MMMM d YYYY");
+  let parisTime = moment().tz("Europe/Paris").format(" h:mm:ss");
+  let parisAmPm = moment().tz("Europe/Paris").format(" A");
 
-parisDateElement.innerHTML = parisDate;
+  parisDateElement.innerHTML = parisDate;
 
-parisTimeElement.innerHTML = `${parisTime} <small>${parisAmPm}</small>`;
+  parisTimeElement.innerHTML = `${parisTime} <small>${parisAmPm}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
